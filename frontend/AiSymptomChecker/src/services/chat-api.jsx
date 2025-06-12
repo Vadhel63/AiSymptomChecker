833b0ded769662d6bfd5f3026843c283adc00240
Vadhel63 <vadhelmilan45@gmail.com>
2025-06-12 16:51:59 +0530

    Url updated

--- frontend/AiSymptomChecker/src/services/chat-api.jsx
+++ frontend/AiSymptomChecker/src/services/chat-api.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://aisymptomchecker.onrender.com/";
+const API_BASE_URL = "https://aisymptomchecker.onrender.com";
 
 export const chatAPI = {
   // Get all conversations for a user
